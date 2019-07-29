d25a19d550f50a32c9253bad45ab5eb2639e2c83
FaceDeer <derksenmobile@gmail.com>
2019-07-28 20:10:25 -0600

    add icons to tables, add per-player option of hiding itemnames, add tooltips

--- formspecs.lua
+++ formspecs.lua
@@ -1,48 +1,104 @@
+local get_icon = function(item)
+	local def = minetest.registered_items[item]
+	if def then
+		local inventory_image = def.inventory_image
+		if inventory_image and inventory_image ~= "" then
+			return inventory_image
+		else
+			local tiles = def.tiles
+			local tilecount = #tiles
+			-- Textures of node; +Y, -Y, +X, -X, +Z, -Z
+			local selected_tile = tiles[math.min(5,tilecount)]
+			if type(selected_tile) == "string" then
+				return selected_tile
+			end
+			local tile_name = selected_tile.name
+			if tile_name then
+				return tile_name
+			end			
+		end
+	end
+	return "unknown_item.png"
+end
+
+
 -- Inventory formspec
 -------------------------------------------------------------------------------------
 
-local inventory_comp = function(invitem1, invitem2) return invitem1.item < invitem2.item end
+local inventory_item_comp = function(invitem1, invitem2) return invitem1.item < invitem2.item end
+local inventory_desc_comp = function(invitem1, invitem2) return invitem1.description < invitem2.description end
 
 local get_account_formspec = function(market, account)
-	local formspec = {
-		"size[10,10]",
-		"tabheader[0,0;tabs;"..market.def.description..",Your Inventory,Market Orders;2;false;true]",
-		"tablecolumns[text;text,align=center;text;text,align=center]",
-		"table[0,0;9.9,4;inventory;",
-		"Item,Quantity,Item,Quantity"
-	}
+	local show_itemnames = account.show_itemnames == "true"
 
 	local inventory = {}
 	local inventory_count = 0
 	for item, quantity in pairs(account.inventory) do
-		table.insert(inventory, {item=item, quantity=quantity})
+		local def = minetest.registered_items[item]
+		local description
+		if def then description = def.description else description = "Unknown Item" end
+		local icon = get_icon(item)
+		table.insert(inventory, {item=item, quantity=quantity, icon=icon, description=description})
 		inventory_count = inventory_count + quantity
 	end
-	table.sort(inventory, inventory_comp)
-	local i = 1
-	while i <= #inventory do
-		local n = #formspec+1
-		formspec[n] = "," .. inventory[i].item
-		formspec[n+1] = "," .. inventory[i].quantity
-		i = i + 1
-		if inventory[i] then
-			formspec[n+2] = "," .. inventory[i].item
-			formspec[n+3] = "," .. inventory[i].quantity
-		end
-		i = i + 1
+	if show_itemnames then
+		table.sort(inventory, inventory_item_comp)
+	else
+		table.sort(inventory, inventory_desc_comp)
+	end
+
+	local formspec = {
+		"size[10,10]",
+		"tabheader[0,0;tabs;"..market.def.description..",Your Inventory,Market Orders;2;false;true]"
+	}
+	formspec[#formspec+1] = "tablecolumns[image"
+	for i=1, #inventory, 2 do
+		formspec[#formspec+1] = ","..i.."="..inventory[i].icon
+	end
+	if show_itemnames then
+		formspec[#formspec+1] = ";text"
+	end
+	formspec[#formspec+1] = ";text;text,align=center;image"
+	for i=2, #inventory, 2 do
+		formspec[#formspec+1] = ","..i.."="..inventory[i].icon
+	end
+	if show_itemnames then
+		formspec[#formspec+1] = ";text"
+	end
+	formspec[#formspec+1] = ";text;text,align=center]"
+	formspec[#formspec+1] = "tooltip[inventory;All the items you've transfered to the market to sell and the items you've\npurchased with buy orders. Double-click on an item to bring it back into your\npersonal inventory.]"
+	formspec[#formspec+1] = "table[0,0;9.9,4;inventory;"
+	formspec[#formspec+1] = "0"
+	if show_itemnames then
+		formspec[#formspec+1] = ",Item"
+	end	
+	formspec[#formspec+1] = ",Description,Quantity,0"
+	if show_itemnames then
+		formspec[#formspec+1] = ",Item"
+	end	
+	formspec[#formspec+1] = ",Description,Quantity"
+
+	for i, entry in ipairs(inventory) do
+		formspec[#formspec+1] = "," .. i
+		if show_itemnames then
+			formspec[#formspec+1] = "," .. entry.item
+		end	
+		formspec[#formspec+1] = "," .. entry.description .. "," .. entry.quantity
 	end	
 	
-	formspec[#formspec+1] = "]container[1.1,4.5]list[detached:commoditymarket:"..market.name..";add;0,0;1,1;]"
+	formspec[#formspec+1] = "]container[1.1,4.5]list[detached:commoditymarket:" .. market.name .. ";add;0,0;1,1;]"
 		.."label[1,0;Drop items here to\nadd to your account]"
-		.."listring[current_player;main]listring[detached:commoditymarket:"..market.name..";add]"
+		.."listring[current_player;main]listring[detached:commoditymarket:" .. market.name .. ";add]"
 		
 	if market.def.inventory_limit then
-		formspec[#formspec+1] = "label[3,0;Inventory limit:\n"..inventory_count.."/"..market.def.inventory_limit.."]"
+		formspec[#formspec+1] = "label[3,0;Inventory limit:\n" .. inventory_count.."/" .. market.def.inventory_limit .. "]"
+			.. "tooltip[3,0;1.5,1;You can still receive purchased items if you've exceeded your inventory limit,\nbut you won't be able to transfer items from your personal inventory into\nthe market until you've emptied it back down below the limit again.]"
 	end
-	formspec[#formspec+1] = "label[4.9,0;Balance:\n"..market.def.currency_symbol .. account.balance .."]"
+	formspec[#formspec+1] = "label[4.9,0;Balance:\n" .. market.def.currency_symbol .. account.balance .. "]"
 		.."field[6.1,0.25;1,1;withdrawamount;;]"
 		.."field_close_on_enter[withdrawamount;false]"
 		.."button[6.7,0;1.2,1;withdraw;Withdraw]"
+		.."tooltip[4.9,0;3.5,1;Enter the amount of currency you'd like to withdraw then click the 'Withdraw'\nbutton to convert it into items and transfer it to your personal inventory.]"
 		.."container_end[]"
 	
 	formspec[#formspec+1] = "container[1.1,5.75]list[current_player;main;0,0;8,1;]"..
@@ -50,6 +106,7 @@ local get_account_formspec = function(market, account)
 
 	return table.concat(formspec)
 end
+
 
 -- Market formspec
 --------------------------------------------------------------------------------------------------------
@@ -73,9 +130,11 @@ end
 local compare_last_price = function(mkt1, mkt2) return (mkt1.last_price or 2^31) < (mkt2.last_price or 2^31) end
 
 local sort_marketlist = function(item_list, account)
-	local sort_by = account.sort_markets_by_column or 1
-	--	"Item,Description,#00FF00,Buy Vol,Buy Max,#FF0000,Sell Vol,Sell Min,Last Price"
-	if sort_by == 1 then
+	local show_itemnames = account.show_itemnames == "true"
+	local sort_by = tonumber(account.sort_markets_by_column) or -1
+	if show_itemnames then sort_by = sort_by - 1 end -- displace columns 1 if itemnames are enabled
+	--	"Icon,Item,Description,#00FF00,Buy Vol,Buy Max,#FF0000,Sell Vol,Sell Min,Last Price"
+	if sort_by == 1 and show_itemnames then
 		table.sort(item_list, compare_market_item)
 	elseif sort_by == 2 then
 		table.sort(item_list, compare_market_desc)
@@ -127,48 +186,76 @@ end
 
 local get_market_formspec = function(market, account)
 	local selected = account.selected
+	local market_list = make_marketlist(market, account)
+	local show_itemnames = account.show_itemnames == "true"
+
 	local formspec = {
 		"size[10,10]",
 		"tabheader[0,0;tabs;"..market.def.description..",Your Inventory,Market Orders;3;false;true]",
-		"tablecolumns[text;"
-			.."text;"
-			.."color,span=2;"
-			.."text,align=right,tooltip=Number of items there's demand for in the market;"
-			.."text,align=right,tooltip=Maximum price being offered to buy one of these;"
-			.."color,span=2;"
-			.."text,align=right,tooltip=Number of items available for sale in the market;"
-			.."text,align=right,tooltip=Minimum price being demanded to sell one of these;"
-			.."text,align=right,tooltip=Price paid for one of these the last time one was sold]",
-		"table[0,0;9.9,5;summary;",
-		"Item,Description,#00FF00,Buy Vol,Buy Max,#FF0000,Sell Vol,Sell Min,Last Price"
+		"tablecolumns["
 	}
+	
+	-- column definitions
+	formspec[#formspec+1] = "image," -- icon
+	for i, row in ipairs(market_list) do
+		if i > 1 then
+			formspec[#formspec+1] = ","
+		end
+		formspec[#formspec+1] = i .. "=" .. get_icon(row.item)
+	end
+	formspec[#formspec+1] = ";"
+	if show_itemnames then
+		formspec[#formspec+1] = "text;" -- itemname
+	end
+	formspec[#formspec+1] = "text;" -- description
+		.."color,span=2;"
+		.."text,align=right,tooltip=Number of items there's demand for in the market;"
+		.."text,align=right,tooltip=Maximum price being offered to buy one of these;"
+		.."color,span=2;"
+		.."text,align=right,tooltip=Number of items available for sale in the market;"
+		.."text,align=right,tooltip=Minimum price being demanded to sell one of these;"
+		.."text,align=right,tooltip=Price paid for one of these the last time one was sold]"
+		.."table[0,0;9.9,5;summary;"
+
+	-- header row
+	formspec[#formspec+1] = "0"-- icon
+	if show_itemnames then
+		formspec[#formspec+1] = ",Item" -- itemname
+	end
+	formspec[#formspec+1] = ",Description,#00FF00,Buy Vol,Buy Max,#FF0000,Sell Vol,Sell Min,Last Price"
 
 	local selected_idx
 	local selected_row
 	
-	local market_list = make_marketlist(market, account)
+	local truncate_length
+	-- If we're not showing item names we can be more generous in allowed description length
+	if show_itemnames then truncate_length = truncate_item_names_to else truncate_length = truncate_item_names_to * 2 end
 	
 	-- Show list of item market summaries
 	for i, row in ipairs(market_list) do
-		local item_display = row.item
-		if item_display:len() > truncate_item_names_to then
-			item_display = item_display:sub(1,truncate_item_names_to-2).."..."
+		formspec[#formspec+1] = ","..i -- icon
+
+		if show_itemnames then
+			local item_display = row.item
+			if item_display:len() > truncate_length then
+				item_display = item_display:sub(1,truncate_length-2).."..."
+			end
+			formspec[#formspec+1] = "," .. item_display
 		end
-		local n = #formspec+1
-		formspec[n] = "," .. item_display
+		
 		local def = minetest.registered_items[row.item] or {}
 		local desc_display = def.description or "Unknown Item"
-		if desc_display:len() > truncate_item_names_to then
-			desc_display = desc_display:sub(1,truncate_item_names_to-2).."..."
+		if desc_display:len() > truncate_length then
+			desc_display = desc_display:sub(1,truncate_length-2).."..."
 		end
-		formspec[n+1] = "," .. desc_display
-		formspec[n+2] = ",#00FF00"
-		formspec[n+3] = "," .. row.buy_volume
-		formspec[n+4] = "," .. ((row.buy_orders[#row.buy_orders] or {}).price or "-")
-		formspec[n+5] = ",#FF0000"
-		formspec[n+6] = "," .. row.sell_volume
-		formspec[n+7] = "," .. ((row.sell_orders[#row.sell_orders] or {}).price or "-")
-		formspec[n+8] = "," .. (row.last_price or "-")
+		formspec[#formspec+1] = "," .. desc_display
+		formspec[#formspec+1] = ",#00FF00"
+		formspec[#formspec+1] = "," .. row.buy_volume
+		formspec[#formspec+1] = "," .. ((row.buy_orders[#row.buy_orders] or {}).price or "-")
+		formspec[#formspec+1] = ",#FF0000"
+		formspec[#formspec+1] = "," .. row.sell_volume
+		formspec[#formspec+1] = "," .. ((row.sell_orders[#row.sell_orders] or {}).price or "-")
+		formspec[#formspec+1] = "," .. (row.last_price or "-")
 		
 		-- we happen to be processing the row that matches the item this player has selected. Record that.
 		if selected == row.item then
@@ -188,6 +275,7 @@ local get_market_formspec = function(market, account)
 		.."field[0,0.85;2.5,1;search_filter;;"..minetest.formspec_escape(account.search or "").."]"
 		.."image_button[2.25,0.6;0.8,0.8;commoditymarket_search.png;apply_search;]"
 		.."checkbox[1.77,0;filter_participating;My orders;".. account.filter_participating .."]"
+		.."tooltip[filter_participating;Select this to show only the markets where you have either a buy or a sell order pending.]"
 		.."tooltip[search_filter;Enter substring to search item identifiers for]"
 		.."tooltip[apply_search;Apply search to outputs]"
 		.."container_end[]"
@@ -195,14 +283,25 @@ local get_market_formspec = function(market, account)
 	-- if a visible item market is selected, show the orders for it in detail
 	if selected_row then
 		local current_time = minetest.get_gametime()
+		
+		local desc_display
+		if show_itemnames then
+			desc_display = selected
+		else
+			local def = minetest.registered_items[selected_row.item] or {}
+			desc_display = def.description or "Unknown Item"
+		end
+
 		-- player inventory for this item and for currency
-		formspec[#formspec+1] = "label[0.1,5.1;"..selected.."\nIn inventory: "
+		formspec[#formspec+1] = "label[0.1,5.1;"..desc_display.."\nIn inventory: "
 			.. tostring(account.inventory[selected] or 0) .."\nBalance: "..market.def.currency_symbol..account.balance .."]"
 		
 		-- buy/sell controls
 		formspec[#formspec+1] = "container[6,5]"
-		formspec[#formspec+1] = "button[0,0.5;1,1;buy;Buy]field[1.3,0.85;1,1;quantity;Quantity;]field[2.3,0.85;1,1;price;Price;]button[3,0.5;1,1;sell;Sell]"
+		formspec[#formspec+1] = "button[0,0.5;1,1;buy;Buy]field[1.3,0.85;1,1;quantity;Quantity;]"
+			.."field[2.3,0.85;1,1;price;Price;]button[3,0.5;1,1;sell;Sell]"
 			.."field_close_on_enter[quantity;false]field_close_on_enter[price;false]"
+			.."tooltip[0,0.25;3.75,1;Use these fields to enter buy and sell orders for the selected item]"
 		formspec[#formspec+1] = "container_end[]"
 
 		-- table of buy and sell orders
@@ -268,6 +367,11 @@ local get_info_formspec = function(market, account)
 	end
 	formspec[#formspec+1] = "]"
 	
+	local show_itemnames = account.show_itemnames or "false"
+
+	formspec[#formspec+1] = "container[0.5, 7.5]label[0,0;Settings:]checkbox[0,0.25;show_itemnames;Show Itemnames;"
+		..show_itemnames.."]container_end[]"
+	
 	return table.concat(formspec)
 end
 
@@ -284,9 +388,9 @@ commoditymarket.get_formspec = function(market, account)
 	end
 end
 
+
 ------------------------------------------------------------------------------------
 -- Handling recieve_fields
-
 
 local add_to_player_inventory = function(name, item, amount)
 	local playerinv = minetest.get_inventory({type="player", name=name})
@@ -397,13 +501,25 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 	if fields.inventory then
 		local invevent = minetest.explode_table_event(fields.inventory)
 		if invevent.type == "DCL" then
-			local index = invevent.row*2 + math.ceil(invevent.column/2) - 4
+			local col_count
+			local show_itemnames = account.show_itemnames == "true"
+			if show_itemnames then
+				col_count = 4
+			else
+				col_count = 3
+			end
+		
+			local index = invevent.row*col_count + math.ceil(invevent.column/col_count) - col_count*2
 			local account = market:get_account(name)
 			local inventory = {}
 			for item, quantity in pairs(account.inventory) do
 				table.insert(inventory, {item=item, quantity=quantity})
 			end
-			table.sort(inventory, inventory_comp)
+			if show_itemnames then
+				table.sort(inventory, inventory_item_comp)
+			else
+				table.sort(inventory, inventory_desc_comp)
+			end
 			if inventory[index] then
 				local item = inventory[index].item
 				local amount = account.inventory[item]
@@ -445,11 +561,17 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 		end
 	end
 
-	if (fields.filter_participating == "true" and account.filter_participating == "false") or
-		(fields.filter_participating == "false" and account.filter_participating == "true") then
-		account.filter_participating = fields.filter_participating
-		something_changed = true
+	local process_checkbox = function(property_name, fields, account)
+		if (fields[property_name] == "true" and account[property_name] ~= "true") or
+			(fields[property_name] == "false" and account[property_name] ~= "false") then
+			account[property_name] = fields[property_name]
+			return true
+		end
+		return false
 	end
+
+	if process_checkbox("filter_participating", fields, account) then something_changed = true end
+	if process_checkbox("show_itemnames", fields, account) then something_changed = true end
 				
 	if fields.apply_search or fields.key_enter_field == "search_filter" then
 		something_changed = true
